439a9c6fe8fe3fb5870e937dbeb038dbaf683ed3
emma tamburello <emmatamburello@emmas-MacBook-Air.local>
2026-03-02 14:23:15 -0500

    mod06

--- mod06_deanonymize.py
+++ mod06_deanonymize.py
@@ -1,3 +1,4 @@
+from sunau import AUDIO_UNKNOWN_SIZE
 import pandas as pd
 
 def load_data(anonymized_path, auxiliary_path):
@@ -18,7 +19,14 @@ def link_records(anon_df, aux_df):
       anon_id, matched_name
     containing ONLY uniquely matched records.
     """
-    raise NotImplementedError
+    quasi_columns = ["age", "gender", "zip3"]
+    merged = pd.merge(anon_df, aux_df, on=quasi_columns)
+
+    uniques = merged[merged.groupby(quasi_columns)["age"].transform("count") == 1]
+
+    anon_id = anon_df.columns[0]
+    aux_id = aux_df.columns[0]
+    return uniques[[anon_id, aux_id]].rename(columns={anon_id: "anon_id", aux_id: "matched_name"})
 
 
 def deanonymization_rate(matches_df, anon_df):
@@ -26,4 +34,6 @@ def deanonymization_rate(matches_df, anon_df):
     Compute the fraction of anonymized records
     that were uniquely re-identified.
     """
-    raise NotImplementedError
+    total = len(anon_df)
+    matched = len(matches_df)
+    return matched/total
